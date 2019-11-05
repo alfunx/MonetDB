4f453ec2900191fcdcf8ea1c3e19ff3584f0a4a9
Alphonse Mariya <alphonse.mariya@hotmail.com>
2019-11-05 21:59:22 +0100

    Restore original formatting of operator_type

--- sql/include/sql_relation.h
+++ sql/include/sql_relation.h
@@ -141,8 +141,8 @@ typedef enum operator_type {
 	op_basetable = 0,
 	op_table,
 	op_ddl,
-	op_project, /* includes order by */
-	op_select,
+	op_project, 		/* includes order by */
+	op_select,	
 	op_join,
 	op_left,
 	op_right,
@@ -153,12 +153,12 @@ typedef enum operator_type {
 	op_union,
 	op_inter,
 	op_except,
-	op_groupby,
+	op_groupby,	
 	op_topn,
 	op_sample,
-	op_insert, /* insert(l=table, r insert expressions) */
-	op_update, /* update(l=table, r update expressions) */
-	op_delete, /* delete(l=table, r delete expression) */
+	op_insert, 	/* insert(l=table, r insert expressions) */ 
+	op_update, 	/* update(l=table, r update expressions) */
+	op_delete, 	/* delete(l=table, r delete expression) */
 
 	op_matrixadd,
 	op_matrixsub,
